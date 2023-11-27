0a6484da7ef82c9b2442900c1e88b882a3215843
burgerboii <alex@vr.dk>
2023-11-27 13:51:30 +0100

    27/11 alex smth

--- DDU projekt 3/Assets/Scripts/ShapesMovement.cs
+++ DDU projekt 3/Assets/Scripts/ShapesMovement.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 
 public class ShapesMovement : MonoBehaviour
 {
-    [SerializeField] GameObject Home;
+    public GameObject Home;
     [SerializeField] float speed;
     [SerializeField] float rotateSpeed;
+
+    bool move;
+    Animator anim;
+    private void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
     void Update()
     {
-        if (gameObject.transform.position != Home.transform.position)
+        if (move)
         {
-            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, Home.transform.position, speed * Time.deltaTime);
-            transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+            if (gameObject.transform.position != Home.transform.position)
+            {
+                gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, Home.transform.position, speed * Time.deltaTime);
+                transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+            }
+            else
+            {
+                Debug.Log("hit home");
+            }
         }
-        else
-        {
-            Debug.Log("hit home");
-        }
+    }
 
+    public void startMoving()
+    {
+        move = true;
+        Invoke("startanim", 3);
+    }
+    void startanim()
+    {
+        anim.SetTrigger("GoGreen");
     }
 }
 
--- DDU projekt 3/Assets/Scripts/TheManager.cs
+++ DDU projekt 3/Assets/Scripts/TheManager.cs
@@ -4,5 +4,29 @@ using UnityEngine;
 
 public class TheManager : MonoBehaviour
 {
-   
+    [SerializeField] GameObject HouseToAttack;
+    [SerializeField] GameObject[] DifferentShapes;
+
+    [SerializeField] Vector2[] startPoints;
+    [SerializeField] float[] timeToSpawn;
+    [SerializeField] int[] whichShapeToSpawn;
+    [SerializeField] GameObject[] ShapesInAction;
+
+    int currentNumber = 0;
+
+    private void Start()
+    {
+        for (int i = 0; i < startPoints.Length; i++)
+        {
+            ShapesInAction[i] = Instantiate(DifferentShapes[whichShapeToSpawn[i]]);
+            ShapesInAction[i].transform.position = startPoints[i];
+            ShapesInAction[i].GetComponent<ShapesMovement>().Home = HouseToAttack;
+            Invoke("SpawnShape", timeToSpawn[i]);
+        }
+    }
+    void SpawnShape()
+    {
+        ShapesInAction[currentNumber].GetComponent<ShapesMovement>().startMoving();
+        currentNumber++;
+    }
 }
