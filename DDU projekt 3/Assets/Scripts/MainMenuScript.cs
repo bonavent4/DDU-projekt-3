f6375c1402f9af9d7ecea58c65a814f4e6b12a08
ValdeMorgen <valdemar.morgen@icloud.com>
2023-12-18 13:58:09 +0100

    MinMenu2

--- DDU projekt 3/Assets/Scripts/MainMenuScript.cs
+++ DDU projekt 3/Assets/Scripts/MainMenuScript.cs
@@ -6,15 +6,12 @@ using UnityEngine;
 public class MainMenuScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject exitButton;
 
     // Update is called once per frame
     void Update()
     {
-        
+        EnableExitButton();
     }
     public void PlaySongScene()
     {
@@ -26,5 +23,27 @@ public class MainMenuScript : MonoBehaviour
         SceneManager.LoadScene(1);
 
     }
+    public void ReturnToMainScene()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+    public void EnableExitButton()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (exitButton.activeSelf)
+            {
+                exitButton.SetActive(false);
+            }
+            else
+            {
+                exitButton.SetActive(true);
+            }
+        }
+        
+
+    }
+
 
 }
