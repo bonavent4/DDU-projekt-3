1a3102d36dc4cea8592363afb6ddeb8edb588d65
ValdeMorgen <valdemar.morgen@icloud.com>
2023-12-18 13:43:00 +0100

    Main Menu

---
+++ DDU projekt 3/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlaySongScene()
+    {
+       SceneManager.LoadScene(2);
+
+    }
+    public void PlayEditScene()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+}
